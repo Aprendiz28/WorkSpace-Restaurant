6a16ec25ad8c35b723773553fa22369c9e19f92f
kenny1303 <arevalosarmientokenny@gmail.com>
2023-11-28 16:49:42 -0500

    arreglo de traductor5

--- login.php
+++ login.php
@@ -40,9 +40,9 @@
                         width="18%"> </a>
                 <div class="collapse navbar-toggleable-md  float-lg-right" id="mainNavbarCollapse">
                     <ul class="nav navbar-nav">
-                        <li class="nav-item"> <a class="nav-link active" href="index.php">Home <span
+                        <li class="nav-item"> <a class="nav-link active" href="index.php">Inicio<span
                                     class="sr-only">(current)</span></a> </li>
-                        <li class="nav-item"> <a class="nav-link active" href="restaurants.php">Restaurants <span
+                        <li class="nav-item"> <a class="nav-link active" href="restaurants.php">Restaurantes <span
                                     class="sr-only"></span></a> </li>
 
                         <?php
@@ -52,8 +52,8 @@
                         } else {
 
 
-                            echo '<li class="nav-item"><a href="your_orders.php" class="nav-link active">My Orders</a> </li>';
-                            echo '<li class="nav-item"><a href="logout.php" class="nav-link active">Logout</a> </li>';
+                            echo '<li class="nav-item"><a href="your_orders.php" class="nav-link active">Mis ordenes</a> </li>';
+                            echo '<li class="nav-item"><a href="logout.php" class="nav-link active">Cerrar sesión</a> </li>';
                         }
 
                         ?>
