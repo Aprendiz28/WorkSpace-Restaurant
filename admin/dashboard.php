ddd1bad7d3d6073a8a67d71f6dd65f25927075b5
Jcarcamo <jcmorales0821@gmail.com>
2023-11-29 14:18:54 -0500

    check

--- admin/dashboard.php
+++ admin/dashboard.php
@@ -9,56 +9,42 @@ if (empty($_SESSION["adm_id"])) {
     header('location:index.php');
 } else {
     ?>
-
-
     <head>
         <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="description" content="">
         <meta name="author" content="">
-        <title>Admin Panel</title>
+        <title>Panel de administrador</title>
         <link href="css/lib/bootstrap/bootstrap.min.css" rel="stylesheet">
         <link href="css/helper.css" rel="stylesheet">
         <link href="css/style.css" rel="stylesheet">
     </head>
-
-
     <body class="fix-header">
-
         <div class="preloader">
             <svg class="circular" viewBox="25 25 50 50">
                 <circle class="path" cx="50" cy="50" r="20" fill="none" stroke-width="2" stroke-miterlimit="10" />
             </svg>
         </div>
-
         <div id="main-wrapper">
-
             <div class="header">
                 <nav class="navbar top-navbar navbar-expand-md navbar-light">
 
                     <div class="navbar-header">
                         <a class="navbar-brand" href="dashboard.php">
-
                             <span><img src="images/icn.png" alt="homepage" class="dark-logo" /></span>
                         </a>
                     </div>
-
                     <div class="navbar-collapse">
                         <ul class="navbar-nav mr-auto mt-md-0">
                         </ul>
-
-
-
-
-
                         <li class="nav-item dropdown">
                             <a class="nav-link dropdown-toggle text-muted  " href="#" data-toggle="dropdown"
                                 aria-haspopup="true" aria-expanded="false"><img src="images/bookingSystem/user0-icn.png"
                                     alt="user" class="profile-pic" /></a>
                             <div class="dropdown-menu dropdown-menu-right animated zoomIn">
                                 <ul class="dropdown-user">
-                                    <li><a href="logout.php"><i class="fa fa-power-off"></i> Logout</a></li>
+                                    <li><a href="logout.php"><i class="fa fa-power-off"></i> Cerrar sesión</a></li>
                                 </ul>
                             </div>
                         </li>
@@ -66,60 +52,48 @@ if (empty($_SESSION["adm_id"])) {
                     </div>
                 </nav>
             </div>
-
             <div class="left-sidebar">
-
                 <div class="scroll-sidebar">
-
-
                     <nav class="sidebar-nav">
                         <ul id="sidebarnav">
                             <li class="nav-devider"></li>
-                            <li class="nav-label">Home</li>
-                            <li> <a href="dashboard.php"><i class="fa fa-tachometer"></i><span>Dashboard</span></a>
+                            <li class="nav-label">Inicio</li>
+                            <li> <a href="dashboard.php"><i class="fa fa-tachometer"></i><span>Panel</span></a>
                             </li>
-                            <li class="nav-label">Log</li>
+                            <li class="nav-label">Registro</li>
                             <li> <a href="all_users.php"> <span><i
-                                            class="fa fa-user f-s-20 "></i></span><span>Users</span></a></li>
+                                            class="fa fa-user f-s-20 "></i></span><span>Usuarios</span></a></li>
                             <li> <a class="has-arrow  " href="#" aria-expanded="false"><i
                                         class="fa fa-archive f-s-20 color-warning"></i><span
-                                        class="hide-menu">Restaurant</span></a>
+                                        class="hide-menu">Restaurante</span></a>
                                 <ul aria-expanded="false" class="collapse">
-                                    <li><a href="all_restaurant.php">All Restaurant</a></li>
-                                    <li><a href="add_category.php">Add Category</a></li>
-                                    <li><a href="add_restaurant.php">Add Restaurant</a></li>
+                                    <li><a href="all_restaurant.php">Todo Restaurante</a></li>
+                                    <li><a href="add_category.php">añadir categoría</a></li>
+                                    <li><a href="add_restaurant.php">Agregar restaurante</a></li>
 
                                 </ul>
                             </li>
                             <li> <a class="has-arrow  " href="#" aria-expanded="false"><i class="fa fa-cutlery"
                                         aria-hidden="true"></i><span class="hide-menu">Menu</span></a>
                                 <ul aria-expanded="false" class="collapse">
-                                    <li><a href="all_menu.php">All Menues</a></li>
-                                    <li><a href="add_menu.php">Add Menu</a></li>
-
-
-
+                                    <li><a href="all_menu.php">Todos los menús</a></li>
+                                    <li><a href="add_menu.php">Agregar menú</a></li>
                                 </ul>
                             </li>
                             <li> <a href="all_orders.php"><i class="fa fa-shopping-cart"
-                                        aria-hidden="true"></i><span>Orders</span></a></li>
-
+                                        aria-hidden="true"></i><span>Pedidos</span></a></li>
                         </ul>
                     </nav>
-
                 </div>
-
             </div>
-
             <div class="page-wrapper">
                 <div class="container-fluid">
                     <div class="col-lg-12">
                         <div class="card card-outline-primary">
                             <div class="card-header">
-                                <h4 class="m-b-0 text-white">Admin Dashboard</h4>
+                                <h4 class="m-b-0 text-white">Panel de administración</h4>
                             </div>
                             <div class="row">
-
                                 <div class="col-md-3">
                                     <div class="card p-30">
                                         <div class="media">
@@ -131,15 +105,13 @@ if (empty($_SESSION["adm_id"])) {
                                                     <?php $sql = "select * from restaurant";
                                                     $result = mysqli_query($db, $sql);
                                                     $rws = mysqli_num_rows($result);
-
                                                     echo $rws; ?>
                                                 </h2>
-                                                <p class="m-b-0">Restaurants</p>
+                                                <p class="m-b-0">Restaurantes</p>
                                             </div>
                                         </div>
                                     </div>
                                 </div>
-
                                 <div class="col-md-3">
                                     <div class="card p-30">
                                         <div class="media">
@@ -154,12 +126,11 @@ if (empty($_SESSION["adm_id"])) {
 
                                                     echo $rws; ?>
                                                 </h2>
-                                                <p class="m-b-0">Dishes</p>
+                                                <p class="m-b-0">Platos</p>
                                             </div>
                                         </div>
                                     </div>
                                 </div>
-
                                 <div class="col-md-3">
                                     <div class="card p-30">
                                         <div class="media">
@@ -171,16 +142,13 @@ if (empty($_SESSION["adm_id"])) {
                                                     <?php $sql = "select * from users";
                                                     $result = mysqli_query($db, $sql);
                                                     $rws = mysqli_num_rows($result);
-
                                                     echo $rws; ?>
                                                 </h2>
-                                                <p class="m-b-0">Users</p>
+                                                <p class="m-b-0">Usuarios</p>
                                             </div>
                                         </div>
                                     </div>
                                 </div>
-
-
                                 <div class="col-md-3">
                                     <div class="card p-30">
                                         <div class="media">
@@ -192,16 +160,14 @@ if (empty($_SESSION["adm_id"])) {
                                                     <?php $sql = "select * from users_orders";
                                                     $result = mysqli_query($db, $sql);
                                                     $rws = mysqli_num_rows($result);
-
                                                     echo $rws; ?>
                                                 </h2>
-                                                <p class="m-b-0">Total Orders</p>
+                                                <p class="m-b-0">Órdenes totales</p>
                                             </div>
                                         </div>
                                     </div>
                                 </div>
                             </div>
-
                             <div class="row">
                                 <div class="col-md-4">
                                     <div class="card p-30">
@@ -214,15 +180,13 @@ if (empty($_SESSION["adm_id"])) {
                                                     <?php $sql = "select * from res_category";
                                                     $result = mysqli_query($db, $sql);
                                                     $rws = mysqli_num_rows($result);
-
                                                     echo $rws; ?>
                                                 </h2>
-                                                <p class="m-b-0">Restro Categories</p>
+                                                <p class="m-b-0">Categorías retro</p>
                                             </div>
                                         </div>
                                     </div>
                                 </div>
-
                                 <div class="col-md-4">
                                     <div class="card p-30">
                                         <div class="media">
@@ -234,15 +198,13 @@ if (empty($_SESSION["adm_id"])) {
                                                     <?php $sql = "select * from users_orders WHERE status = 'in process' ";
                                                     $result = mysqli_query($db, $sql);
                                                     $rws = mysqli_num_rows($result);
-
                                                     echo $rws; ?>
                                                 </h2>
-                                                <p class="m-b-0">Processing Orders</p>
+                                                <p class="m-b-0">Procesamiento de pedidos</p>
                                             </div>
                                         </div>
                                     </div>
                                 </div>
-
                                 <div class="col-md-4">
                                     <div class="card p-30">
                                         <div class="media">
@@ -254,16 +216,14 @@ if (empty($_SESSION["adm_id"])) {
                                                     <?php $sql = "select * from users_orders WHERE status = 'closed' ";
                                                     $result = mysqli_query($db, $sql);
                                                     $rws = mysqli_num_rows($result);
-
                                                     echo $rws; ?>
                                                 </h2>
-                                                <p class="m-b-0">Delivered Orders</p>
+                                                <p class="m-b-0">Pedidos entregados</p>
                                             </div>
                                         </div>
                                     </div>
                                 </div>
                             </div>
-
                             <div class="row">
                                 <div class="col-md-4">
                                     <div class="card p-30">
@@ -276,15 +236,13 @@ if (empty($_SESSION["adm_id"])) {
                                                     <?php $sql = "select * from users_orders WHERE status = 'rejected' ";
                                                     $result = mysqli_query($db, $sql);
                                                     $rws = mysqli_num_rows($result);
-
                                                     echo $rws; ?>
                                                 </h2>
-                                                <p class="m-b-0">Cancelled Orders</p>
+                                                <p class="m-b-0">Pedidos cancelados</p>
                                             </div>
                                         </div>
                                     </div>
                                 </div>
-
                                 <div class="col-md-4">
                                     <div class="card p-30">
                                         <div class="media">
@@ -300,7 +258,7 @@ if (empty($_SESSION["adm_id"])) {
                                                     echo $sum;
                                                     ?>
                                                 </h2>
-                                                <p class="m-b-0">Total Earnings</p>
+                                                <p class="m-b-0">Ganancias Totales</p>
                                             </div>
                                         </div>
                                     </div>
@@ -309,9 +267,7 @@ if (empty($_SESSION["adm_id"])) {
                         </div>
                     </div>
                 </div>
-
                 <?php include "include/footer.php" ?>
-
                 <script src="js/lib/jquery/jquery.min.js"></script>
                 <script src="js/lib/bootstrap/js/popper.min.js"></script>
                 <script src="js/lib/bootstrap/js/bootstrap.min.js"></script>
@@ -319,9 +275,7 @@ if (empty($_SESSION["adm_id"])) {
                 <script src="js/sidebarmenu.js"></script>
                 <script src="js/lib/sticky-kit-master/dist/sticky-kit.min.js"></script>
                 <script src="js/custom.min.js"></script>
-
     </body>
-
     </html>
     <?php
 }
