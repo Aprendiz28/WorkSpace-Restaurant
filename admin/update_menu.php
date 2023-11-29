45cfadf71c87c40c7f279de6af659fd8bb2be4ad
Jcarcamo <jcmorales0821@gmail.com>
2023-11-29 14:58:46 -0500

    restaurant

--- admin/update_menu.php
+++ admin/update_menu.php
@@ -4,17 +4,8 @@
 include("../connection/connect.php");
 error_reporting(0);
 session_start();
-
-
-
-
 if (isset($_POST['submit'])) //if upload btn is pressed
 {
-
-
-
-
-
 
 
     if (empty($_POST['d_name']) || empty($_POST['about']) || $_POST['price'] == '' || $_POST['res_name'] == '') {
@@ -67,24 +58,9 @@ if (isset($_POST['submit'])) //if upload btn is pressed
 
     }
 
-
-
-
-
-
-
 }
 
-
-
-
-
-
-
-
 ?>
-
-
 
 <head>
     <meta charset="utf-8">
@@ -93,24 +69,19 @@ if (isset($_POST['submit'])) //if upload btn is pressed
     <meta name="description" content="">
     <meta name="author" content="">
     <link rel="icon" type="image/png" sizes="16x16" href="images/favicon.png">
-    <title>Update Menu</title>
+    <title>Menú de actualización</title>
     <link href="css/lib/bootstrap/bootstrap.min.css" rel="stylesheet">
     <link href="css/helper.css" rel="stylesheet">
     <link href="css/style.css" rel="stylesheet">
 
 </head>
-
 <body class="fix-header">
-
     <div class="preloader">
         <svg class="circular" viewBox="25 25 50 50">
             <circle class="path" cx="50" cy="50" r="20" fill="none" stroke-width="2" stroke-miterlimit="10" />
         </svg>
     </div>
-
-
     <div id="main-wrapper">
-
         <div class="header">
             <nav class="navbar top-navbar navbar-expand-md navbar-light">
                 <div class="navbar-header">
@@ -120,7 +91,6 @@ if (isset($_POST['submit'])) //if upload btn is pressed
                     </a>
                 </div>
                 <div class="navbar-collapse">
-
                     <ul class="navbar-nav mr-auto mt-md-0">
                     </ul>
 
@@ -131,7 +101,7 @@ if (isset($_POST['submit'])) //if upload btn is pressed
                                     alt="user" class="profile-pic" /></a>
                             <div class="dropdown-menu dropdown-menu-right animated zoomIn">
                                 <ul class="dropdown-user">
-                                    <li><a href="logout.php"><i class="fa fa-power-off"></i> Logout</a></li>
+                                    <li><a href="logout.php"><i class="fa fa-power-off"></i> Cerrar sesión</a></li>
                                 </ul>
                             </div>
                         </li>
@@ -139,8 +109,6 @@ if (isset($_POST['submit'])) //if upload btn is pressed
                 </div>
             </nav>
         </div>
-
-
         <div class="left-sidebar">
 
             <div class="scroll-sidebar">
@@ -148,14 +116,14 @@ if (isset($_POST['submit'])) //if upload btn is pressed
                 <nav class="sidebar-nav">
                     <ul id="sidebarnav">
                         <li class="nav-devider"></li>
-                        <li class="nav-label">Home</li>
-                        <li> <a href="dashboard.php"><i class="fa fa-tachometer"></i><span>Dashboard</span></a></li>
-                        <li class="nav-label">Log</li>
+                        <li class="nav-label">Inicio</li>
+                        <li> <a href="dashboard.php"><i class="fa fa-tachometer"></i><span>Panel</span></a></li>
+                        <li class="nav-label">Registro</li>
                         <li> <a href="all_users.php"> <span><i
-                                        class="fa fa-user f-s-20 "></i></span><span>Users</span></a></li>
+                                        class="fa fa-user f-s-20 "></i></span><span>Usuarios</span></a></li>
                         <li> <a class="has-arrow  " href="#" aria-expanded="false"><i
                                     class="fa fa-archive f-s-20 color-warning"></i><span
-                                    class="hide-menu">Restaurant</span></a>
+                                    class="hide-menu">Restaurante</span></a>
                             <ul aria-expanded="false" class="collapse">
                                 <li><a href="all_restaurant.php">All Restaurants</a></li>
                                 <li><a href="add_category.php">Add Category</a></li>
@@ -166,14 +134,14 @@ if (isset($_POST['submit'])) //if upload btn is pressed
                         <li> <a class="has-arrow  " href="#" aria-expanded="false"><i class="fa fa-cutlery"
                                     aria-hidden="true"></i><span class="hide-menu">Menu</span></a>
                             <ul aria-expanded="false" class="collapse">
-                                <li><a href="all_menu.php">All Menues</a></li>
-                                <li><a href="add_menu.php">Add Menu</a></li>
+                                <li><a href="all_menu.php">Todos los menús</a></li>
+                                <li><a href="add_menu.php">Agregar menú</a></li>
 
 
                             </ul>
                         </li>
                         <li> <a href="all_orders.php"><i class="fa fa-shopping-cart"
-                                    aria-hidden="true"></i><span>Orders</span></a></li>
+                                    aria-hidden="true"></i><span>Pedidos</span></a></li>
 
                     </ul>
                 </nav>
@@ -196,7 +164,7 @@ if (isset($_POST['submit'])) //if upload btn is pressed
                 <div class="col-lg-12">
                     <div class="card card-outline-primary">
                         <div class="card-header">
-                            <h4 class="m-b-0 text-white">Add Menu to Restaurant</h4>
+                            <h4 class="m-b-0 text-white">Agregar menú al restaurante</h4>
                         </div>
                         <div class="card-body">
                             <form action='' method='post' enctype="multipart/form-data">
@@ -209,7 +177,7 @@ if (isset($_POST['submit'])) //if upload btn is pressed
                                     <div class="row p-t-20">
                                         <div class="col-md-6">
                                             <div class="form-group">
-                                                <label class="control-label">Dish Name</label>
+                                                <label class="control-label">Nombre del plato</label>
                                                 <input type="text" name="d_name" value="<?php echo $roww['title']; ?>"
                                                     class="form-control" placeholder="Morzirella">
                                             </div>
@@ -222,9 +190,7 @@ if (isset($_POST['submit'])) //if upload btn is pressed
                                                     class="form-control form-control-danger" placeholder="slogan">
                                             </div>
                                         </div>
-
                                     </div>
-
                                     <div class="row p-t-20">
                                         <div class="col-md-6">
                                             <div class="form-group">
@@ -236,67 +202,46 @@ if (isset($_POST['submit'])) //if upload btn is pressed
 
                                         <div class="col-md-6">
                                             <div class="form-group has-danger">
-                                                <label class="control-label">Image</label>
+                                                <label class="control-label">Imagen</label>
                                                 <input type="file" name="file" id="lastName"
                                                     class="form-control form-control-danger" placeholder="12n">
                                             </div>
                                         </div>
                                     </div>
-
-
-
                                     <div class="row">
-
-
-
-
-
-
-
-
                                         <div class="col-md-12">
                                             <div class="form-group">
-                                                <label class="control-label">Select Category</label>
+                                                <label class="control-label">selecciona una categoría</label>
                                                 <select name="res_name" class="form-control custom-select"
                                                     data-placeholder="Choose a Category" tabindex="1">
-                                                    <option>--Select Restaurant--</option>
+                                                    <option>--Seleccionar Restaurante--</option>
                                                     <?php $ssql = "select * from restaurant";
                                                     $res = mysqli_query($db, $ssql);
                                                     while ($row = mysqli_fetch_array($res)) {
                                                         echo ' <option value="' . $row['rs_id'] . '">' . $row['title'] . '</option>';
                                                         ;
                                                     }
-
                                                     ?>
                                                 </select>
                                             </div>
                                         </div>
-
-
-
                                     </div>
 
                                 </div>
                         </div>
                         <div class="form-actions">
                             <input type="submit" name="submit" class="btn btn-primary" value="Save">
-                            <a href="all_menu.php" class="btn btn-inverse">Cancel</a>
+                            <a href="all_menu.php" class="btn btn-inverse">Cancelado</a>
                         </div>
                         </form>
                     </div>
                 </div>
             </div>
-
             <?php include "include/footer.php" ?>
-
         </div>
-
     </div>
-
     </div>
-
     </div>
-
     <script src="js/lib/jquery/jquery.min.js"></script>
     <script src="js/lib/bootstrap/js/popper.min.js"></script>
     <script src="js/lib/bootstrap/js/bootstrap.min.js"></script>
@@ -304,8 +249,5 @@ if (isset($_POST['submit'])) //if upload btn is pressed
     <script src="js/sidebarmenu.js"></script>
     <script src="js/lib/sticky-kit-master/dist/sticky-kit.min.js"></script>
     <script src="js/custom.min.js"></script>
-
 </body>
-
-
 </html>
