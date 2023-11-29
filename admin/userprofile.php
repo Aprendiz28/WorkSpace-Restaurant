7ba87c59b363c6bad8a5bc8e1996bf59c6fc0c3a
Jcarcamo <jcmorales0821@gmail.com>
2023-11-29 14:54:07 -0500

    restaurant

--- admin/userprofile.php
+++ admin/userprofile.php
@@ -14,22 +14,17 @@ if (strlen($_SESSION['user_id']) == 0) {
         $sql = mysqli_query($db, "update users_orders set status='$status' where o_id='$form_id'");
 
         echo "<script>alert('form details updated successfully');</script>";
-
     }
-
     ?>
     <script language="javascript" type="text/javascript">
         function f2() {
             window.close();
         }
         ser
-
         function f3() {
             window.print();
         }
     </script>
-
-
     <head>
         <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -37,7 +32,7 @@ if (strlen($_SESSION['user_id']) == 0) {
         <meta name="description" content="">
         <meta name="author" content="">
         <link rel="icon" type="image/png" sizes="16x16" href="images/favicon.png">
-        <title>User Profile</title>
+        <title>Perfil del usuario</title>
         <link href="css/lib/bootstrap/bootstrap.min.css" rel="stylesheet">
         <link href="css/helper.css" rel="stylesheet">
         <link href="css/style.css" rel="stylesheet">
@@ -47,19 +42,15 @@ if (strlen($_SESSION['user_id']) == 0) {
             .indent-small {
                 margin-left: 5px;
             }
-
             .form-group.internal {
                 margin-bottom: 0;
             }
-
             .dialog-panel {
                 margin: 10px;
             }
-
             .datepicker-dropdown {
                 z-index: 200 !important;
             }
-
             .panel-body {
                 background: #e5e5e5;
                 background: -moz-radial-gradient(center, ellipse cover, #e5e5e5 0%, #ffffff 100%);
@@ -71,29 +62,24 @@ if (strlen($_SESSION['user_id']) == 0) {
                 filter: progid:DXImageTransform.Microsoft.gradient(startColorstr='#e5e5e5', endColorstr='#ffffff', GradientType=1);
                 font: 600 15px "Open Sans", Arial, sans-serif;
             }
-
             label.control-label {
                 font-weight: 600;
                 color: #777;
             }
-
             table {
                 width: 650px;
                 border-collapse: collapse;
                 margin: auto;
                 margin-top: 50px;
             }
-
             tr:nth-of-type(odd) {
                 background: #eee;
             }
-
             th {
                 background: #004684;
                 color: white;
                 font-weight: bold;
             }
-
             td,
             th {
                 padding: 10px;
@@ -103,37 +89,22 @@ if (strlen($_SESSION['user_id']) == 0) {
             }
         </style>
     </head>
-
     <body>
 
         <div style="margin-left:50px;">
             <form name="updateticket" id="updatecomplaint" method="post">
-
-
-
-
                 <table border="0" cellspacing="0" cellpadding="0">
-
                     <?php
-
                     $ret1 = mysqli_query($db, "select * FROM users_orders where o_id='" . $_GET['newform_id'] . "'");
                     $ro = mysqli_fetch_array($ret1);
                     $ret2 = mysqli_query($db, "select * FROM users where u_id='" . $ro['u_id'] . "'");
-
                     while ($row = mysqli_fetch_array($ret2)) {
                         ?>
-
-
-
-
                         <tr>
                             <td colspan="2"><b>
                                     <?php echo $row['f_name']; ?>'s profile
                                 </b></td>
-
                         </tr>
-
-
                         <tr>
                             <td>&nbsp;</td>
                             <td>&nbsp;</td>
@@ -144,7 +115,6 @@ if (strlen($_SESSION['user_id']) == 0) {
                                 <?php echo htmlentities($row['date']); ?>
                             </td>
                         </tr>
-
                         <tr height="50">
                             <td><b>First Name:</b></td>
                             <td>
@@ -157,33 +127,19 @@ if (strlen($_SESSION['user_id']) == 0) {
                                 <?php echo htmlentities($row['l_name']); ?>
                             </td>
                         </tr>
-
-
-
                         <tr height="50">
-                            <td><b>User Email:</b></td>
+                            <td><b>Correo electrónico del usuario:</b></td>
                             <td>
                                 <?php echo htmlentities($row['email']); ?>
                             </td>
                         </tr>
 
                         <tr height="50">
-                            <td><b>User Phone:</b></td>
+                            <td><b>Teléfono de usuario</b></td>
                             <td>
                                 <?php echo htmlentities($row['phone']); ?>
                             </td>
                         </tr>
-
-
-
-
-
-
-
-
-
-
-
                         <tr height="50">
                             <td><b>Status:</b></td>
                             <td>
@@ -195,27 +151,17 @@ if (strlen($_SESSION['user_id']) == 0) {
                                 ?>
                             </td>
                         </tr>
-
                         <tr>
-
                             <td colspan="2">
                                 <input name="Submit2" type="submit" class="btn btn-danger" value="Close this window "
                                     onClick="return f2();" style="cursor: pointer;" />
                             </td>
                         </tr>
-
                     <?php }
-
-
                     ?>
-
-
                 </table>
             </form>
         </div>
-
     </body>
-
     </html>
-
 <?php } ?>
--- admin/view_order.php
+++ admin/view_order.php
@@ -4,26 +4,21 @@
 include("../connection/connect.php");
 error_reporting(0);
 session_start();
-
 ?>
-
 <head>
-
-
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="description" content="">
     <meta name="author" content="">
     <link rel="icon" type="image/png" sizes="16x16" href="images/favicon.png">
-    <title>View Order</title>
+    <title>Ver pedido</title>
     <link href="css/lib/bootstrap/bootstrap.min.css" rel="stylesheet">
     <link href="css/helper.css" rel="stylesheet">
     <link href="css/style.css" rel="stylesheet">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
     <script language="javascript" type="text/javascript">
         var popUpWin = 0;
-
         function popUpWindow(URLStr, left, top, width, height) {
             if (popUpWin) {
                 if (!popUpWin.closed) popUpWin.close();
@@ -32,7 +27,6 @@ session_start();
         }
     </script>
 </head>
-
 <body class="fix-header fix-sidebar">
 
     <div class="preloader">
@@ -40,38 +34,28 @@ session_start();
             <circle class="path" cx="50" cy="50" r="20" fill="none" stroke-width="2" stroke-miterlimit="10" />
         </svg>
     </div>
-
     <div id="main-wrapper">
-
         <div class="header">
             <nav class="navbar top-navbar navbar-expand-md navbar-light">
                 <div class="navbar-header">
                     <a class="navbar-brand" href="dashboard.php">
-
                         <span><img src="images/icn.png" alt="homepage" class="dark-logo" /></span>
                     </a>
                 </div>
                 <div class="navbar-collapse">
-
                     <ul class="navbar-nav mr-auto mt-md-0">
-
                     </ul>
-
                     <ul class="navbar-nav my-lg-0">
-
-
-
                         <li class="nav-item dropdown">
 
                             <div class="dropdown-menu dropdown-menu-right mailbox animated zoomIn">
                                 <ul>
                                     <li>
-                                        <div class="drop-title">Notifications</div>
+                                        <div class="drop-title">Notificaciones</div>
                                     </li>
 
                                     <li>
-                                        <a class="nav-link text-center" href="javascript:void(0);"> <strong>Check all
-                                                notifications</strong> <i class="fa fa-angle-right"></i> </a>
+                                        <a class="nav-link text-center" href="javascript:void(0);"> <strong>Consulta todas las notificaciones</strong> <i class="fa fa-angle-right"></i> </a>
                                     </li>
                                 </ul>
                             </div>
@@ -84,7 +68,7 @@ session_start();
                                     alt="user" class="profile-pic" /></a>
                             <div class="dropdown-menu dropdown-menu-right animated zoomIn">
                                 <ul class="dropdown-user">
-                                    <li><a href="logout.php"><i class="fa fa-power-off"></i> Logout</a></li>
+                                    <li><a href="logout.php"><i class="fa fa-power-off"></i> Cerrar sesión</a></li>
                                 </ul>
                             </div>
                         </li>
@@ -100,55 +84,44 @@ session_start();
                 <nav class="sidebar-nav">
                     <ul id="sidebarnav">
                         <li class="nav-devider"></li>
-                        <li class="nav-label">Home</li>
-                        <li> <a href="dashboard.php"><i class="fa fa-tachometer"></i><span>Dashboard</span></a></li>
-                        <li class="nav-label">Log</li>
+                        <li class="nav-label">Inicio</li>
+                        <li> <a href="dashboard.php"><i class="fa fa-tachometer"></i><span>Panel</span></a></li>
+                        <li class="nav-label">Registro</li>
                         <li> <a href="all_users.php"> <span><i
-                                        class="fa fa-user f-s-20 "></i></span><span>Users</span></a></li>
+                                        class="fa fa-user f-s-20 "></i></span><span>Usuarios</span></a></li>
                         <li> <a class="has-arrow  " href="#" aria-expanded="false"><i
                                     class="fa fa-archive f-s-20 color-warning"></i><span
-                                    class="hide-menu">Restaurant</span></a>
+                                    class="hide-menu">Restaurante</span></a>
                             <ul aria-expanded="false" class="collapse">
-                                <li><a href="all_restaurant.php">All Restaurants</a></li>
-                                <li><a href="add_category.php">Add Category</a></li>
-                                <li><a href="add_restaurant.php">Add Restaurant</a></li>
-
+                                <li><a href="all_restaurant.php">Todos los restaurantes</a></li>
+                                <li><a href="add_category.php">añadir categoría</a></li>
+                                <li><a href="add_restaurant.php">Agregar restaurante</a></li>
                             </ul>
                         </li>
                         <li> <a class="has-arrow  " href="#" aria-expanded="false"><i class="fa fa-cutlery"
                                     aria-hidden="true"></i><span class="hide-menu">Menu</span></a>
                             <ul aria-expanded="false" class="collapse">
-                                <li><a href="all_menu.php">All Menues</a></li>
+                                <li><a href="all_menu.php">Todos los menús</a></li>
                                 <li><a href="add_menu.php">Add Menu</a></li>
 
 
                             </ul>
                         </li>
                         <li> <a href="all_orders.php"><i class="fa fa-shopping-cart"
-                                    aria-hidden="true"></i><span>Orders</span></a></li>
-
+                                    aria-hidden="true"></i><span>Pedidos</span></a></li>
                     </ul>
                 </nav>
-
             </div>
-
-
         </div>
-
         <div class="page-wrapper">
-
             <div class="container-fluid">
-
                 <div class="row">
                     <div class="col-12">
-
-
                         <div class="col-lg-12">
                             <div class="card card-outline-primary">
                                 <div class="card-header">
-                                    <h4 class="m-b-0 text-white">View Order</h4>
+                                    <h4 class="m-b-0 text-white">Ver pedido</h4>
                                 </div>
-
                                 <div class="table-responsive m-t-20">
                                     <table id="myTable" class="table table-bordered table-striped">
 
@@ -157,15 +130,9 @@ session_start();
                                             $sql = "SELECT users.*, users_orders.* FROM users INNER JOIN users_orders ON users.u_id=users_orders.u_id where o_id='" . $_GET['user_upd'] . "'";
                                             $query = mysqli_query($db, $sql);
                                             $rows = mysqli_fetch_array($query);
-
-
-
                                             ?>
-
-
-
                                             <tr>
-                                                <td><strong>Username:</strong></td>
+                                                <td><strong>Nombre de usuario:</strong></td>
                                                 <td>
                                                     <center>
                                                         <?php echo $rows['username']; ?>
@@ -176,12 +143,9 @@ session_start();
                                                         <a href="javascript:void(0);"
                                                             onClick="popUpWindow('order_update.php?form_id=<?php echo htmlentities($rows['o_id']); ?>');"
                                                             title="Update order">
-                                                            <button type="button" class="btn btn-primary">Update Order
-                                                                Status</button></a>
+                                                            <button type="button" class="btn btn-primary">Actualizar estado del pedido</button></a>
                                                     </center>
                                                 </td>
-
-
                                             </tr>
                                             <tr>
                                                 <td><strong>Title:</strong></td>
@@ -195,22 +159,17 @@ session_start();
                                                         <a href="javascript:void(0);"
                                                             onClick="popUpWindow('userprofile.php?newform_id=<?php echo htmlentities($rows['o_id']); ?>');"
                                                             title="Update order">
-                                                            <button type="button" class="btn btn-primary">View User
-                                                                Detials</button></a>
-
+                                                            <button type="button" class="btn btn-primary">Ver detalles del usuario</button></a>
                                                     </center>
                                                 </td>
-
                                             </tr>
                                             <tr>
-                                                <td><strong>Quantity:</strong></td>
+                                                <td><strong>Cantidad:</strong></td>
                                                 <td>
                                                     <center>
                                                         <?php echo $rows['quantity']; ?>
                                                     </center>
                                                 </td>
-
-
                                             </tr>
                                             <tr>
                                                 <td><strong>Price:</strong></td>
@@ -224,7 +183,7 @@ session_start();
 
                                             </tr>
                                             <tr>
-                                                <td><strong>Address:</strong></td>
+                                                <td><strong>DIRECCIÓN:</strong></td>
                                                 <td>
                                                     <center>
                                                         <?php echo $rows['address']; ?>
@@ -234,7 +193,7 @@ session_start();
 
                                             </tr>
                                             <tr>
-                                                <td><strong>Date:</strong></td>
+                                                <td><strong>Fecha:</strong></td>
                                                 <td>
                                                     <center>
                                                         <?php echo $rows['date']; ?>
@@ -252,15 +211,14 @@ session_start();
                                                     <td>
                                                         <center><button type="button" class="btn btn-info"><span
                                                                     class="fa fa-bars" aria-hidden="true"></span>
-                                                                Dispatch</button></center>
+                                                                    Despacho</button></center>
                                                     </td>
                                                 <?php
                                                 }
                                                 if ($status == "in process") { ?>
                                                     <td>
                                                         <center><button type="button" class="btn btn-warning"><span
-                                                                    class="fa fa-cog fa-spin" aria-hidden="true"></span>On a
-                                                                Way!</button></center>
+                                                                    class="fa fa-cog fa-spin" aria-hidden="true"></span>¡En una forma!</button></center>
                                                     </td>
                                                     <?php
                                                 }
@@ -269,7 +227,7 @@ session_start();
                                                     <td>
                                                         <center><button type="button" class="btn btn-primary"><span
                                                                     class="fa fa-check-circle" aria-hidden="true"></span>
-                                                                Delivered</button></center>
+                                                                    Entregado</button></center>
                                                     </td>
                                                 <?php
                                                 }
@@ -279,30 +237,14 @@ session_start();
                                                     ?>
                                                     <td>
                                                         <center><button type="button" class="btn btn-danger"> <i
-                                                                    class="fa fa-close"></i> Cancelled</button> </center>
+                                                                    class="fa fa-close"></i> Cancelado</button> </center>
                                                     </td>
                                                 <?php
                                                 }
                                                 ?>
 
 
-                                            </tr>
-
-
-
-
-                                            <!-- /*!
-* Author Name: MH RONY.
-* GigHub Link: https://github.com/dev-mhrony
-* Facebook Link:https://www.facebook.com/dev.mhrony
-* Youtube Link: https://www.youtube.com/channel/UChYhUxkwDNialcxj-OFRcDw
-for any PHP, Laravel, Python, Dart, Flutter work contact me at developer.mhrony@gmail.com
-* Visit My Website : developerrony.com
-
-*/ -->
-
-
-
+                                            </tr> 
                                         </tbody>
                                     </table>
                                 </div>
@@ -314,15 +256,10 @@ for any PHP, Laravel, Python, Dart, Flutter work contact me at developer.mhrony@
             </div>
         </div>
     </div>
-
     </div>
-
     <footer class="footer"> © 2022 - Online Food Ordering System </footer>
-
     </div>
-
     </div>
-
     <script src="js/lib/jquery/jquery.min.js"></script>
     <script src="js/lib/bootstrap/js/popper.min.js"></script>
     <script src="js/lib/bootstrap/js/bootstrap.min.js"></script>
@@ -340,5 +277,4 @@ for any PHP, Laravel, Python, Dart, Flutter work contact me at developer.mhrony@
     <script src="js/lib/datatables/cdn.datatables.net/buttons/1.2.2/js/buttons.print.min.js"></script>
     <script src="js/lib/datatables/datatables-init.js"></script>
 </body>
-
 </html>
