f022f80a32565bd323d807dd38a0aa877a29758d
Jcarcamo <jcmorales0821@gmail.com>
2023-11-29 14:12:47 -0500

    2

--- admin/all_orders.php
+++ admin/all_orders.php
@@ -4,10 +4,7 @@
 include("../connection/connect.php");
 error_reporting(0);
 session_start();
-
 ?>
-
-
 <head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -15,71 +12,48 @@ session_start();
     <meta name="description" content="">
     <meta name="author" content="">
     <link rel="icon" type="image/png" sizes="16x16" href="images/favicon.png">
-    <title>All Orders</title>
+    <title>Todas las órdenes</title>
     <link href="css/lib/bootstrap/bootstrap.min.css" rel="stylesheet">
     <link href="css/helper.css" rel="stylesheet">
     <link href="css/style.css" rel="stylesheet">
-
-
-
 </head>
-
 <body class="fix-header fix-sidebar">
-
     <div class="preloader">
         <svg class="circular" viewBox="25 25 50 50">
             <circle class="path" cx="50" cy="50" r="20" fill="none" stroke-width="2" stroke-miterlimit="10" />
         </svg>
     </div>
-
     <div id="main-wrapper">
-
         <div class="header">
             <nav class="navbar top-navbar navbar-expand-md navbar-light">
                 <div class="navbar-header">
                     <a class="navbar-brand" href="dashboard.php">
-
                         <span><img src="images/icn.png" alt="homepage" class="dark-logo" /></span>
                     </a>
                 </div>
                 <div class="navbar-collapse">
-
                     <ul class="navbar-nav mr-auto mt-md-0">
-
-
-
-
-
                     </ul>
-
                     <ul class="navbar-nav my-lg-0">
-
-
-
                         <li class="nav-item dropdown">
-
                             <div class="dropdown-menu dropdown-menu-right mailbox animated zoomIn">
                                 <ul>
                                     <li>
-                                        <div class="drop-title">Notifications</div>
+                                        <div class="drop-title">Notificaciones</div>
                                     </li>
-
                                     <li>
-                                        <a class="nav-link text-center" href="javascript:void(0);"> <strong>Check all
-                                                notifications</strong> <i class="fa fa-angle-right"></i> </a>
+                                        <a class="nav-link text-center" href="javascript:void(0);"> <strong>Consulta todas las notificaciones</strong> <i class="fa fa-angle-right"></i> </a>
                                     </li>
                                 </ul>
                             </div>
                         </li>
-
-
                         <li class="nav-item dropdown">
                             <a class="nav-link dropdown-toggle text-muted  " href="#" data-toggle="dropdown"
                                 aria-haspopup="true" aria-expanded="false"><img src="images/bookingSystem/user0-icn.png"
                                     alt="user" class="profile-pic" /></a>
                             <div class="dropdown-menu dropdown-menu-right animated zoomIn">
                                 <ul class="dropdown-user">
-                                    <li><a href="logout.php"><i class="fa fa-power-off"></i> Logout</a></li>
+                                    <li><a href="logout.php"><i class="fa fa-power-off"></i> Cerrar sesión</a></li>
                                 </ul>
                             </div>
                         </li>
@@ -87,90 +61,69 @@ session_start();
                 </div>
             </nav>
         </div>
-
         <div class="left-sidebar">
-
-
             <div class="scroll-sidebar">
-
                 <nav class="sidebar-nav">
                     <ul id="sidebarnav">
                         <li class="nav-devider"></li>
-                        <li class="nav-label">Home</li>
-                        <li> <a href="dashboard.php"><i class="fa fa-tachometer"></i><span>Dashboard</span></a></li>
-                        <li class="nav-label">Log</li>
+                        <li class="nav-label">Inicio</li>
+                        <li> <a href="dashboard.php"><i class="fa fa-tachometer"></i><span>Panel</span></a></li>
+                        <li class="nav-label">Registro</li>
                         <li> <a href="all_users.php"> <span><i
-                                        class="fa fa-user f-s-20 "></i></span><span>Users</span></a></li>
+                                        class="fa fa-user f-s-20 "></i></span><span>Usuarios</span></a></li>
                         <li> <a class="has-arrow  " href="#" aria-expanded="false"><i
                                     class="fa fa-archive f-s-20 color-warning"></i><span
-                                    class="hide-menu">Restaurant</span></a>
+                                    class="hide-menu">Restaurante</span></a>
                             <ul aria-expanded="false" class="collapse">
-                                <li><a href="all_restaurant.php">All Restaurants</a></li>
-                                <li><a href="add_category.php">Add Category</a></li>
-                                <li><a href="add_restaurant.php">Add Restaurant</a></li>
-
+                                <li><a href="all_restaurant.php">Todos los restaurantes</a></li>
+                                <li><a href="add_category.php">añadir categoría</a></li>
+                                <li><a href="add_restaurant.php">Agregar restaurante</a></li>
                             </ul>
                         </li>
                         <li> <a class="has-arrow  " href="#" aria-expanded="false"><i class="fa fa-cutlery"
                                     aria-hidden="true"></i><span class="hide-menu">Menu</span></a>
                             <ul aria-expanded="false" class="collapse">
-                                <li><a href="all_menu.php">All Menues</a></li>
-                                <li><a href="add_menu.php">Add Menu</a></li>
-
-
-
+                                <li><a href="all_menu.php">Todos los menús</a></li>
+                                <li><a href="add_menu.php">Agregar menú</a></li>
                             </ul>
                         </li>
                         <li> <a href="all_orders.php"><i class="fa fa-shopping-cart"
-                                    aria-hidden="true"></i><span>Orders</span></a></li>
-
+                                    aria-hidden="true"></i><span>Pedidos</span></a></li>
                     </ul>
                 </nav>
-
             </div>
-
         </div>
-
         <div class="page-wrapper">
             <div class="container-fluid">
-
                 <div class="row">
                     <div class="col-12">
-
-
                         <div class="col-lg-12">
                             <div class="card card-outline-primary">
                                 <div class="card-header">
                                     <h4 class="m-b-0 text-white">All Orders</h4>
                                 </div>
-
                                 <div class="table-responsive m-t-40">
                                     <table id="myTable" class="table table-bordered table-striped">
                                         <thead class="thead-dark">
                                             <tr>
-                                                <th>User</th>
-                                                <th>Title</th>
-                                                <th>Quantity</th>
-                                                <th>Price</th>
-                                                <th>Address</th>
-                                                <th>Status</th>
-                                                <th>Reg-Date</th>
-                                                <th>Action</th>
-
+                                                <th>Usuarios</th>
+                                                <th>Título</th>
+                                                <th>Cantidad</th>
+                                                <th>Precio</th>
+                                                <th>DIRECCIÓN</th>
+                                                <th>Estado</th>
+                                                <th>Fecha de registro</th>
+                                                <th>Accion</th>
                                             </tr>
                                         </thead>
                                         <tbody>
-
-
                                             <?php
                                             $sql = "SELECT users.*, users_orders.* FROM users INNER JOIN users_orders ON users.u_id=users_orders.u_id ";
                                             $query = mysqli_query($db, $sql);
-
                                             if (!mysqli_num_rows($query) > 0) {
                                                 echo '<td colspan="8"><center>No Orders</center></td>';
                                             } else {
                                                 while ($rows = mysqli_fetch_array($query)) {
-
                                                     ?>
                                                     <?php
                                                     echo ' <tr>
@@ -222,40 +175,23 @@ session_start();
                                                         echo '<a href="view_order.php?user_upd=' . $rows['o_id'] . '" " class="btn btn-info btn-flat btn-addon btn-sm m-b-10 m-l-5"><i class="fa fa-edit"></i></a>
 																									</td>
 																									</tr>';
-
-
-
                                                 }
                                             }
-
-
                                             ?>
-
-
-
                                         </tbody>
                                     </table>
                                 </div>
                             </div>
                         </div>
                     </div>
-
-
                 </div>
             </div>
         </div>
     </div>
-
     </div>
-
-
-
     <?php include "include/footer.php" ?>
-
     </div>
-
     </div>
-
     <script src="js/lib/jquery/jquery.min.js"></script>
     <script src="js/lib/bootstrap/js/popper.min.js"></script>
     <script src="js/lib/bootstrap/js/bootstrap.min.js"></script>
@@ -271,7 +207,5 @@ session_start();
     <script src="js/lib/datatables/cdn.rawgit.com/bpampuch/pdfmake/0.1.18/build/vfs_fonts.js"></script>
     <script src="js/lib/datatables/cdn.datatables.net/buttons/1.2.2/js/buttons.html5.min.js"></script>
     <script src="js/lib/datatables/cdn.datatables.net/buttons/1.2.2/js/buttons.print.min.js"></script>
-
 </body>
-
 </html>
