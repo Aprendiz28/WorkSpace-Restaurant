cf7cbbf5710a25ea412b9370737e38ffcaed88d4
Jcarcamo <jcmorales0821@gmail.com>
2023-11-29 15:02:50 -0500

    restaurant

--- admin/update_users.php
+++ admin/update_users.php
@@ -136,72 +136,52 @@
                                 <nav class="sidebar-nav">
                                     <ul id="sidebarnav">
                                         <li class="nav-devider"></li>
-                                        <li class="nav-label">Home</li>
-                                        <li> <a href="dashboard.php"><i class="fa fa-tachometer"></i><span>Dashboard</span></a></li>
-                                        <li class="nav-label">Log</li>
-                                        <li> <a href="all_users.php"> <span><i class="fa fa-user f-s-20 "></i></span><span>Users</span></a></li>
+                                        <li class="nav-label">Inicio</li>
+                                        <li> <a href="dashboard.php"><i class="fa fa-tachometer"></i><span>Panel</span></a></li>
+                                        <li class="nav-label">Registro</li>
+                                        <li> <a href="all_users.php"> <span><i class="fa fa-user f-s-20 "></i></span><span>Usuarios</span></a></li>
                                         <li> <a class="has-arrow  " href="#" aria-expanded="false"><i class="fa fa-archive f-s-20 color-warning"></i><span class="hide-menu">Restaurant</span></a>
                                             <ul aria-expanded="false" class="collapse">
-                                                <li><a href="all_restaurant.php">All Restaurants</a></li>
-                                                <li><a href="add_category.php">Add Category</a></li>
-                                                <li><a href="add_restaurant.php">Add Restaurant</a></li>
+                                                <li><a href="all_restaurant.php">Todos los restaurantes</a></li>
+                                                <li><a href="add_category.php">
+añadir categoría</a></li>
+                                                <li><a href="add_restaurant.php">Agregar restaurante</a></li>
 
                                             </ul>
                                         </li>
                                         <li> <a class="has-arrow  " href="#" aria-expanded="false"><i class="fa fa-cutlery" aria-hidden="true"></i><span class="hide-menu">Menu</span></a>
                                             <ul aria-expanded="false" class="collapse">
-                                                <li><a href="all_menu.php">All Menues</a></li>
-                                                <li><a href="add_menu.php">Add Menu</a></li>
+                                                <li><a href="all_menu.php">
+Todos los menús</a></li>
+                                                <li><a href="add_menu.php">Agregar menu</a></li>
 
    
 
                                             </ul>
                                         </li>
-                                        <li> <a href="all_orders.php"><i class="fa fa-shopping-cart" aria-hidden="true"></i><span>Orders</span></a></li>
-
-
+                                        <li> <a href="all_orders.php"><i class="fa fa-shopping-cart" aria-hidden="true"></i><span>Pedidos</span></a></li>
                                     </ul>
                                 </nav>
-
                             </div>
-
                         </div>
-
                         <div class="page-wrapper" style="height:1200px;">
     
                             <div class="row page-titles">
                                 <div class="col-md-5 align-self-center">
-                                    <h3 class="text-primary">Dashboard</h3>
+                                    <h3 class="text-primary">Panel</h3>
                                 </div>
-
                             </div>
-  
-
-
                             <div class="container-fluid">
-
                                 <div class="row">
-
-
-
                                     <div class="container-fluid">
-
-
-
                                         <?php
                                         echo $error;
                                         echo $success;
-
-
-
                                         ?>
-
-
-
                                         <div class="col-lg-12">
                                             <div class="card card-outline-primary">
                                                 <div class="card-header">
-                                                    <h4 class="m-b-0 text-white">Update Users</h4>
+                                                    <h4 class="m-b-0 text-white">Actualizar usuarios</h4>
                                                 </div>
                                                 <div class="card-body">
                                                     <?php $ssql = "select * from users where u_id='$_GET[user_upd]'";
@@ -214,14 +194,14 @@
                                                             <div class="row p-t-20">
                                                                 <div class="col-md-6">
                                                                     <div class="form-group">
-                                                                        <label class="control-label">Username</label>
+                                                                        <label class="control-label">Nombre de usuario</label>
                                                                         <input type="text" name="uname" class="form-control" value="<?php echo $newrow['username']; ?>" placeholder="username">
                                                                     </div>
                                                                 </div>
 
                                                                 <div class="col-md-6">
                                                                     <div class="form-group has-danger">
-                                                                        <label class="control-label">First-Name</label>
+                                                                        <label class="control-label">Nombre de pila</label>
                                                                         <input type="text" name="fname" class="form-control form-control-danger" value="<?php echo $newrow['f_name']; ?>" placeholder="jon">
                                                                     </div>
                                                                 </div>
@@ -232,7 +212,7 @@
                                                             <div class="row p-t-20">
                                                                 <div class="col-md-6">
                                                                     <div class="form-group">
-                                                                        <label class="control-label">Last-Name </label>
+                                                                        <label class="control-label">Apellido </label>
                                                                         <input type="text" name="lname" class="form-control" placeholder="doe" value="<?php echo $newrow['l_name']; ?>">
                                                                     </div>
                                                                 </div>
@@ -249,56 +229,33 @@
                                                             <div class="row">
                                                                 <div class="col-md-6">
                                                                     <div class="form-group">
-                                                                        <label class="control-label">Password</label>
+                                                                        <label class="control-label">Contraseña</label>
                                                                         <input type="text" name="password" class="form-control form-control-danger" value="<?php echo $newrow['password']; ?>" placeholder="password">
                                                                     </div>
                                                                 </div>
 
                                                                 <div class="col-md-6">
                                                                     <div class="form-group">
-                                                                        <label class="control-label">Phone</label>
+                                                                        <label class="control-label">Celular</label>
                                                                         <input type="text" name="phone" class="form-control form-control-danger" value="<?php echo $newrow['phone']; ?>" placeholder="phone">
                                                                     </div>
                                                                 </div>
                                                             </div>
-
-
-                                                    
-
                                                         </div>
                                                 </div>
                                                 <div class="form-actions">
                                                     <input type="submit" name="submit" class="btn btn-primary" value="Save">
-                                                    <a href="all_users.php" class="btn btn-inverse">Cancel</a>
+                                                    <a href="all_users.php" class="btn btn-inverse">Cancelado</a>
                                                 </div>
                                                 </form>
                                             </div>
                                         </div>
                                     </div>
-
-   
-
-
-
-
-
-
-
-
-
-
-
                                 </div>
-
                             </div>
-
-
                             <?php include "include/footer.php" ?>
-
                         </div>
-
                     </div>
-
                     <script src="js/lib/jquery/jquery.min.js"></script>
                     <script src="js/lib/bootstrap/js/popper.min.js"></script>
                     <script src="js/lib/bootstrap/js/bootstrap.min.js"></script>
@@ -306,8 +263,6 @@
                     <script src="js/sidebarmenu.js"></script>
                     <script src="js/lib/sticky-kit-master/dist/sticky-kit.min.js"></script>
                     <script src="js/custom.min.js"></script>
-
                 </body>
-
                 </html>
   
