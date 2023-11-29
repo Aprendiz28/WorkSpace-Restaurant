ea57927164769806e072120d111e93dd2f951b18
Jcarcamo <jcmorales0821@gmail.com>
2023-11-29 13:57:52 -0500

    2

--- admin/add_category.php
+++ admin/add_category.php
@@ -11,7 +11,6 @@ if (isset($_POST['submit'])) {
 																<strong>field Required!</strong>
 															</div>';
     } else {
-
         $check_cat = mysqli_query($db, "SELECT c_name FROM res_category where c_name = '" . $_POST['c_name'] . "' ");
 
 
@@ -22,32 +21,26 @@ if (isset($_POST['submit'])) {
 																<strong>Category already exist!</strong>
 															</div>';
         } else {
-
-
             $mql = "INSERT INTO res_category(c_name) VALUES('" . $_POST['c_name'] . "')";
             mysqli_query($db, $mql);
             $success = '<div class="alert alert-success alert-dismissible fade show">
 																<button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>
 																New Category Added Successfully.</br></div>';
-
         }
     }
 }
 ?>
-
 <head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="description" content="">
     <meta name="author" content="">
-    <title>Add Category</title>
+    <title>Añadir categoria</title>
     <link href="css/lib/bootstrap/bootstrap.min.css" rel="stylesheet">
     <link href="css/helper.css" rel="stylesheet">
     <link href="css/style.css" rel="stylesheet">
-
 </head>
-
 <body class="fix-header">
     <div class="preloader">
         <svg class="circular" viewBox="25 25 50 50">
@@ -55,12 +48,10 @@ if (isset($_POST['submit'])) {
         </svg>
     </div>
     <div id="main-wrapper">
-
         <div class="header">
             <nav class="navbar top-navbar navbar-expand-md navbar-light">
                 <div class="navbar-header">
                     <a class="navbar-brand" href="dashboard.php">
-
                         <span><img src="images/icn.png" alt="homepage" class="dark-logo" /></span>
                     </a>
                 </div>
@@ -74,7 +65,7 @@ if (isset($_POST['submit'])) {
                                     alt="user" class="profile-pic" /></a>
                             <div class="dropdown-menu dropdown-menu-right animated zoomIn">
                                 <ul class="dropdown-user">
-                                    <li><a href="logout.php"><i class="fa fa-power-off"></i> Logout</a></li>
+                                    <li><a href="logout.php"><i class="fa fa-power-off"></i>Cerrar sesión</a></li>
                                 </ul>
                             </div>
                         </li>
@@ -83,38 +74,33 @@ if (isset($_POST['submit'])) {
             </nav>
         </div>
         <div class="left-sidebar">
-
             <div class="scroll-sidebar">
-
                 <nav class="sidebar-nav">
                     <ul id="sidebarnav">
                         <li class="nav-devider"></li>
-                        <li class="nav-label">Home</li>
-                        <li> <a href="dashboard.php"><i class="fa fa-tachometer"></i><span>Dashboard</span></a></li>
-                        <li class="nav-label">Log</li>
+                        <li class="nav-label">Inicio</li>
+                        <li> <a href="dashboard.php"><i class="fa fa-tachometer"></i><span>Panel</span></a></li>
+                        <li class="nav-label">Registro</li>
                         <li> <a href="all_users.php"> <span><i
-                                        class="fa fa-user f-s-20 "></i></span><span>Users</span></a></li>
+                                        class="fa fa-user f-s-20 "></i></span><span>Usuarios</span></a></li>
                         <li> <a class="has-arrow  " href="#" aria-expanded="false"><i
                                     class="fa fa-archive f-s-20 color-warning"></i><span
-                                    class="hide-menu">Restaurant</span></a>
+                                    class="hide-menu">Restaurante</span></a>
                             <ul aria-expanded="false" class="collapse">
-                                <li><a href="all_restaurant.php">All Restaurants</a></li>
-                                <li><a href="add_category.php">Add Category</a></li>
-                                <li><a href="add_restaurant.php">Add Restaurant</a></li>
-
+                                <li><a href="all_restaurant.php">Todos los restaurantes</a></li>
+                                <li><a href="add_category.php">Añadir categoria</a></li>
+                                <li><a href="add_restaurant.php">Agregar restaurante</a></li>
                             </ul>
                         </li>
                         <li> <a class="has-arrow  " href="#" aria-expanded="false"><i class="fa fa-cutlery"
                                     aria-hidden="true"></i><span class="hide-menu">Menu</span></a>
                             <ul aria-expanded="false" class="collapse">
-                                <li><a href="all_menu.php">All Menues</a></li>
-                                <li><a href="add_menu.php">Add Menu</a></li>
-
-
+                                <li><a href="all_menu.php">Todos los menús</a></li>
+                                <li><a href="add_menu.php">Agregar menú</a></li>
                             </ul>
                         </li>
                         <li> <a href="all_orders.php"><i class="fa fa-shopping-cart"
-                                    aria-hidden="true"></i><span>Orders</span></a></li>
+                                    aria-hidden="true"></i><span>Pedidos</span></a></li>
                     </ul>
                 </nav>
             </div>
@@ -129,7 +115,7 @@ if (isset($_POST['submit'])) {
                         <div class="col-lg-12">
                             <div class="card card-outline-primary">
                                 <div class="card-header">
-                                    <h4 class="m-b-0 text-white">Add Restaurant Category</h4>
+                                    <h4 class="m-b-0 text-white">Agregar categoría de restaurante</h4>
                                 </div>
                                 <form action='' method='post'>
                                     <div class="form-body">
@@ -138,16 +124,14 @@ if (isset($_POST['submit'])) {
                                         <div class="row p-t-20">
                                             <div class="col-md-12">
                                                 <div class="form-group">
-                                                    <label class="control-label">Category</label>
+                                                    <label class="control-label">Categoría</label>
                                                     <input type="text" name="c_name" class="form-control">
                                                 </div>
                                             </div>
-
-
                                         </div>
                                         <div class="form-actions">
                                             <input type="submit" name="submit" class="btn btn-primary" value="Save">
-                                            <a href="add_category.php" class="btn btn-inverse">Cancel</a>
+                                            <a href="add_category.php" class="btn btn-inverse">Cancelar</a>
                                         </div>
                                 </form>
                             </div>
@@ -157,15 +141,15 @@ if (isset($_POST['submit'])) {
                 <div class="col-12">
                     <div class="card">
                         <div class="card-body">
-                            <h4 class="card-title">Listed Categories</h4>
+                            <h4 class="card-title">Categorías listadas</h4>
                             <div class="table-responsive m-t-40">
                                 <table id="myTable" class="table table-bordered table-hover table-striped">
                                     <thead class="thead-dark">
                                         <tr>
                                             <th>ID</th>
-                                            <th>Category Name</th>
-                                            <th>Date</th>
-                                            <th>Action</th>
+                                            <th>Nombre de la categoría</th>
+                                            <th>Fecha</th>
+                                            <th>Acción</th>
                                         </tr>
                                     </thead>
                                     <tbody>
@@ -205,5 +189,4 @@ if (isset($_POST['submit'])) {
     <script src="js/lib/sticky-kit-master/dist/sticky-kit.min.js"></script>
     <script src="js/custom.min.js"></script>
 </body>
-
 </html>
