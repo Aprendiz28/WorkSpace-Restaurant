fba801d2a7e12977057df884bc17290881a49229
Jcarcamo <jcmorales0821@gmail.com>
2023-11-29 13:32:19 -0500

    subo

--- admin/all_users.php
+++ admin/all_users.php
@@ -16,7 +16,7 @@ session_start();
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="description" content="">
     <meta name="author" content="">
-    <title>All Users</title>
+    <title>Todos los usuarios</title>
     <link href="css/lib/bootstrap/bootstrap.min.css" rel="stylesheet">
     <link href="css/helper.css" rel="stylesheet">
     <link href="css/style.css" rel="stylesheet">
