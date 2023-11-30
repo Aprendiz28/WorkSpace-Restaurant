fa25f6a6025bc91cd53d9c010c2c01d36ebbe123
julsk12 <jpsierra316@misena.edu.co>
2023-11-30 14:36:51 -0500

    aa

--- registration.php
+++ registration.php
@@ -85,7 +85,7 @@ if (isset($_POST['submit'])) {
                             <?php
                             if (empty($_SESSION["user_id"])) {
                                 echo '<li class="nav-item"><a href="login.php" class="nav-link active">Login</a> </li>
-							  <li class="nav-item"><a href="registration.php" class="nav-link active">Register</a> </li>';
+							  <li class="nav-item"><a href="registration.php" class="nav-link active">Registrar</a> </li>';
                             } else {
 
 
